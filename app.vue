9cef69af2eacc9f4af294a5becde49aa860e34b0
Brandon Taylor <brandondtaylor01@gmail.com>
2024-06-28 15:41:08 -0400

    Added CloudFlare Web Analytics

--- app.vue
+++ app.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
-  useHeadSafe({
+  useHead({
     title: 'Brandon Taylor | brandontaylor.dev',
     htmlAttrs: {
       class: 'bg-slate-100 dark:bg-slate-900 overflow-x-hidden'
-    }
+    },
+    script: [
+      !process.dev? {
+        'src': 'https://static.cloudflareinsights.com/beacon.min.js',
+        'data-cf-beacon': '{"token": "1b2831f35e1140dbb0adf5f5c8c5d7ce"}',
+        defer: true
+      } : {}
+    ]
   })
 </script>
 
