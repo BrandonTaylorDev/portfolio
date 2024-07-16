4e04ee29bb829a3e2ce8d23f47acbbc24116a00b
Brandon Taylor <brandondtaylor01@gmail.com>
2024-07-16 18:32:30 -0400

    Adds K8s dialog

---
+++ components/Hyperlink.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  type Props = {
+    href?: string
+  }
+  const props = withDefaults(defineProps<Props>(), {
+    
+  })
+</script>
+
+<template>
+  <component :is="props ? 'a' : 'span'" :href="props.href" class="text-teal-600 hover:text-teal-400 dark:text-teal-400 hover:dark:text-teal-600 transition-colors duration-200 hover:underline cursor-pointer">
+    <slot />
+  </component>
+</template>
