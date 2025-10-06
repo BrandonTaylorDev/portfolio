16e413264e4df587ed9976e4d7de6eaf96af0e3e
Brandon Taylor <11229429+BrandonTaylorDev@users.noreply.github.com>
2025-10-06 05:43:22 -0400

    Update RailControl.vue

--- components/RailControl.vue
+++ components/RailControl.vue
@@ -9,50 +9,49 @@ const emit = defineEmits(['click'])
   <div
     :class="[
       'absolute right-0 top-1/2 -translate-y-1/2 h-screen w-4',
-      // animate transform + bg color (+ shadow if you like)
-      'transition-[transform,background-color,box-shadow] duration-700 ease-in-out',
+      'transform-gpu will-change-transform transition-[transform,background-color,box-shadow] duration-700 ease-in-out',
       showResume
         ? '-translate-x-[calc(100vw-1rem)] bg-zinc-300 shadow-2xl'
         : 'translate-x-0 bg-zinc-700 shadow-none'
     ]"
   >
-    <div
-      :class="[
-        'absolute top-1/2 -translate-y-1/2 w-8 h-16 overflow-hidden right-4',
-        // animate radius, slide, bg color
-        'transition-[border-radius,transform,background-color] duration-700 ease-in-out',
-        showResume
-          ? 'rounded-r-full translate-x-[calc(100%+1rem)] bg-zinc-300'
-          : 'rounded-l-full translate-x-0 bg-zinc-700'
-      ]"
-    >
-      <button
-        class="flex items-center h-full w-full text-2xl"
-        @click="$emit('click')"
+    <!-- NON-ANIMATING WRAPPER: handles vertical centering only -->
+    <div class="absolute right-4 top-1/2 -translate-y-1/2">
+      <!-- INNER: animates X, radius, and bg -->
+      <div
+        :class="[
+          'w-8 h-16 overflow-hidden',
+          'transform-gpu will-change-[transform,background-color] transition-[border-radius,transform,background-color] duration-700 ease-in-out',
+          showResume
+            ? 'rounded-r-full translate-x-[calc(100%+1rem)] bg-zinc-300'
+            : 'rounded-l-full translate-x-0 bg-zinc-700'
+        ]"
       >
-        <span
-          :style="{ '--g': showResume ? 0 : 1 }"
-          class="basis-0 [flex-grow:var(--g)] transition-[flex-grow] duration-700 ease-in-out"
-        />
-
-        <div
-          :class="[
-            'shrink-0 transition-[transform,color] duration-700 ease-in-out',
-            showResume ? 'rotate-180' : ''
-          ]"
+        <button
+          class="flex items-center h-full w-full text-2xl"
+          @click="$emit('click')"
         >
-          <svg
-            width="24" height="24" viewBox="0 0 24 24" fill="none"
-            class="text-black"
+          <!-- Spacer grows 0 -> 1 to push chevron right when showResume is true -->
+          <span
+            :style="{ '--g': showResume ? 0 : 1 }"
+            class="basis-0 [flex-grow:var(--g)] transition-[flex-grow] duration-700 ease-in-out"
+          />
+
+          <div
+            :class="[
+              'shrink-0 transform-gpu transition-[transform,color] duration-700 ease-in-out',
+              showResume
+                ? 'rotate-180 text-zinc-900'
+                : 'text-zinc-100'
+            ]"
           >
-            <path
-              d="M15 6L9 12L15 18"
-              stroke="currentColor" stroke-width="1.5"
-              stroke-linecap="round" stroke-linejoin="round"
-            />
-          </svg>
-        </div>
-      </button>
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+              <path d="M15 6L9 12L15 18" stroke="currentColor" stroke-width="1.5"
+                    stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+          </div>
+        </button>
+      </div>
     </div>
   </div>
 </template>
